ce19c1b7e5ff43bf6bb3162cff8565c7f998812e
Sergei Komarov <anaxronik@gmail.com>
2023-10-18 17:02:18 +0700

    add multiply file

--- src/components/JsonUploader/JsonUploader.tsx
+++ src/components/JsonUploader/JsonUploader.tsx
@@ -7,24 +7,33 @@ interface IProps {
 
 const JsonUploader: React.FC<IProps> = observer((props) => {
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files) {
-      const fileReader = new FileReader();
-      fileReader.readAsText(e.target.files[0], "UTF-8");
-      fileReader.onload = (e) => {
-        const file = e.target?.result;
-        if (file) {
-          try {
-            const json = JSON.parse(file as string);
-            props.onUploadJson(json);
-          } catch (error) {
-            console.error("Ошибка при парсинге json");
+    const files = e.target.files;
+
+    if (files) {
+      Object.values(files)?.forEach((file: Blob) => {
+        const fileReader = new FileReader();
+        fileReader.readAsText(file, "UTF-8");
+        fileReader.onload = (e) => {
+          const file = e.target?.result;
+          if (file) {
+            try {
+              const json = JSON.parse(file as string);
+              props.onUploadJson(json);
+            } catch (error) {
+              console.error("Ошибка при парсинге json");
+            }
           }
-        }
-      };
+        };
+      });
     }
   };
   return (
-    <input type="file" onChange={handleFileChange} accept="application/JSON" />
+    <input
+      type="file"
+      onChange={handleFileChange}
+      accept="application/JSON"
+      multiple
+    />
   );
 });
 
